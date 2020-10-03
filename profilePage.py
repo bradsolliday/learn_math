fc1c6f618782ec811a69e203a326b2b2bf4ed377
Bradley-Solliday <bradley@DESKTOP-3JLURJ2.localdomain>
2020-10-03 01:10:26 -0400

    Replace func profile_list with class ProfileList
    -In profilePage.py
    -Also added .swp files to .gitignore

--- profilePage.py
+++ profilePage.py
@@ -2,55 +2,61 @@ import tkinter as tk
 
 # Returns the root of a button which loads a profile when clicked
 def profile_button(master, name):
-	from windowController import load_overview_page
+    from windowController import load_overview_page
 
-	return tk.Button(
-				master=master,
-				text=name,
-				relief=tk.RAISED,
-				borderwidth=4,
-				command=lambda: load_overview_page(master, name))
+    return tk.Button(
+                master=master,
+                text=name,
+                relief=tk.RAISED,
+                borderwidth=4,
+                command=lambda: load_overview_page(master, name))
 
 
+class ProfileList(tk.Frame):
+    
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
 
-# Returns the root frame of the list of profile buttons
-def profile_list(master):
-	from dataManager import profile_list
+        self.configure(relief=tk.RIDGE, bg="yellow", borderwidth=5)
+        self.columnconfigure(0, weight=1, minsize=80)
 
-	profiles = profile_list()
+        from dataManager import profile_list
+        profile_names = profile_list()
 
-	root = tk.Frame(master=master, relief=tk.RIDGE, bg="yellow", borderwidth=5)
-	root.columnconfigure(0, weight=1, minsize=80)
+        self.buttons = [None] * len(profile_names)
 
-	for row, profile_name in enumerate(profiles):
-		button = profile_button(root, profile_name)
-		button.grid(row=row, column=0, stick=tk.E+tk.W, padx=3, pady=3)
-
-	return root
+        for row, name in enumerate(profile_names):
+            self.buttons[row] = profile_button(self, name)
+            self.buttons[row].grid(
+                                row=row,
+                                column=0,
+                                stick=tk.E+tk.W,
+                                padx=3,
+                                pady=3)
 
 # Returns the root frame of the profile page
 def profile_page(master):
-	from windowController import load_create_profile_page
+    from windowController import load_create_profile_page
 
-	root = tk.Frame(master=master)
+    root = tk.Frame(master=master)
 
-	root.columnconfigure(0, weight=1, minsize=20)
-	root.columnconfigure(1, weight=1, minsize=100)
-	root.columnconfigure(2, weight=1, minsize=20)
+    root.columnconfigure(0, weight=1, minsize=20)
+    root.columnconfigure(1, weight=1, minsize=100)
+    root.columnconfigure(2, weight=1, minsize=20)
 
-	fastmath_label = tk.Label(master=root, text="Fast Math")
-	fastmath_label.grid(row=0, column=1)
+    fastmath_label = tk.Label(master=root, text="Fast Math")
+    fastmath_label.grid(row=0, column=1)
 
-	select_profile_label = tk.Label(master=root, text="Select Profile")
-	select_profile_label.grid(row=1, column=1, sticky=tk.W)
+    select_profile_label = tk.Label(master=root, text="Select Profile")
+    select_profile_label.grid(row=1, column=1, sticky=tk.W)
 
-	profiles = profile_list(root)
-	profiles.grid(row=2, column=1, sticky=tk.E+tk.W)
+    profiles = ProfileList(master=root)
+    profiles.grid(row=2, column=1, sticky=tk.E+tk.W)
 
-	create_profile_button = tk.Button(
-		master=root,
-		text="Create New Profile",
-		command=lambda: load_create_profile_page(master))
-	create_profile_button.grid(row=3, column=1, sticky=tk.W+tk.E)
+    create_profile_button = tk.Button(
+        master=root,
+        text="Create New Profile",
+        command=lambda: load_create_profile_page(master))
+    create_profile_button.grid(row=3, column=1, sticky=tk.W+tk.E)
 
-	return root
+    return root
