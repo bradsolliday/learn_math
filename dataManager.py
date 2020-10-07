6d6281c302011a92420d70234ab1c88fa3221b46
Bradley-Solliday <bradley@DESKTOP-3JLURJ2.localdomain>
2020-10-07 18:34:20 -0400

    Changed profile button to class, add delete option
    -Also fixed some indenting to 4 spaces

--- dataManager.py
+++ dataManager.py
@@ -5,61 +5,65 @@ PROFILES_DIR = "saved_profiles"
 
 class Profile:
 
-	# Loads and returns existing profile of string profile_name
-	def __init__(self, profile_name):
-		self.profile_name = profile_name
+    # Loads and returns existing profile of string profile_name
+    def __init__(self, profile_name):
+        self.profile_name = profile_name
 
-		with open(os.path.join(PROFILES_DIR, profile_name + ".txt"), "r") as f:
-			addition_data, multiplication_data = f.read().split("|")[1::2]
+        with open(os.path.join(PROFILES_DIR, profile_name + ".txt"), "r") as f:
+            addition_data, multiplication_data = f.read().split("|")[1::2]
 
-		def extract_data(data):
-			output = [[0] * 12] * 12
-			#for operand1, operand2, score in data.split("\n")[0:-1]:
-			for triple in data.split("\n")[0:-1]:
-				operand1, operand2, score = triple.split(",")
-				output[int(operand1)][int(operand2)] = float(score)
+        def extract_data(data):
+            output = [[0] * 12] * 12
+            #for operand1, operand2, score in data.split("\n")[0:-1]:
+            for triple in data.split("\n")[0:-1]:
+                operand1, operand2, score = triple.split(",")
+                output[int(operand1)][int(operand2)] = float(score)
 
-		self.addition = extract_data(addition_data)
-		self.multiplication = extract_data(multiplication_data)
+        self.addition = extract_data(addition_data)
+        self.multiplication = extract_data(multiplication_data)
 
 
 
-	# Creates a new profile with name profile_name
-	@classmethod
-	def new(cls, profile_name):
-		obj = cls.__new__(cls)
-		obj.profile_name = os.path.split(profile_name)[1]
-		obj.addition = [[MIN_SCORE for oper2 in range(1, 13)]
-							for oper1 in range(1, 13)]
-		obj.multiplication = obj.addition.copy()
-		return obj
+    # Creates a new profile with name profile_name
+    @classmethod
+    def new(cls, profile_name):
+        obj = cls.__new__(cls)
+        obj.profile_name = os.path.split(profile_name)[1]
+        obj.addition = [[MIN_SCORE for oper2 in range(1, 13)]
+                            for oper1 in range(1, 13)]
+        obj.multiplication = obj.addition.copy()
+        return obj
 
-	# Saves profile to "saved_profiles/[profile_name]"
-	def save(self):
-		relative_path = os.path.join(PROFILES_DIR, self.profile_name + ".txt")
-		if not os.path.exists(os.path.dirname(relative_path)):
-			os.makedirs(os.path.dirname(relative_path))
+    # Saves profile to "saved_profiles/[profile_name]"
+    def save(self):
+        relative_path = os.path.join(PROFILES_DIR, self.profile_name + ".txt")
+        if not os.path.exists(os.path.dirname(relative_path)):
+            os.makedirs(os.path.dirname(relative_path))
 
-		def write_scores(file, score_list):
-			for operand1, op2_list in enumerate(score_list):
-				for operand2, score in enumerate(op2_list):
-					file.write(f"{operand1},{operand2},{score}\n")
+        def write_scores(file, score_list):
+            for operand1, op2_list in enumerate(score_list):
+                for operand2, score in enumerate(op2_list):
+                    file.write(f"{operand1},{operand2},{score}\n")
 
-		with open(relative_path, "w") as f:
-			f.write("Addition Scores (operand1,operand2,score)\n|")
-			write_scores(f, self.addition)
-			f.write("|multiplication Scores (operand1,operand2,score)\n|")
-			write_scores(f, self.multiplication)
+        with open(relative_path, "w") as f:
+            f.write("Addition Scores (operand1,operand2,score)\n|")
+            write_scores(f, self.addition)
+            f.write("|multiplication Scores (operand1,operand2,score)\n|")
+            write_scores(f, self.multiplication)
 
 def profile_list():
-	if os.path.isdir(PROFILES_DIR):
-		return [os.path.splitext(f)[0] for f in os.listdir(PROFILES_DIR)
-					if os.path.isfile(os.path.join(PROFILES_DIR, f))]
-	return []
+    if os.path.isdir(PROFILES_DIR):
+        return [os.path.splitext(f)[0] for f in os.listdir(PROFILES_DIR)
+                    if os.path.isfile(os.path.join(PROFILES_DIR, f))]
+    return []
 
 
 # Creates a new save file with name new_profile_name
 # precondition: new_profile_name is not the empty string
 def create_profile(new_profile_name):
-	new_profile = Profile.new(new_profile_name)
-	new_profile.save()
+    new_profile = Profile.new(new_profile_name)
+    new_profile.save()
+
+def delete_profile(profile_name):
+    os.remove(os.path.join(PROFILES_DIR, profile_name + ".txt"))
+
--- profilePage.py
+++ profilePage.py
@@ -5,15 +5,40 @@ def profile_button(master, name):
     from windowController import load_overview_page
 
     return tk.Button(
-                master=master,
-                text=name,
-                relief=tk.RAISED,
-                borderwidth=4,
-                command=lambda: load_overview_page(master, name))
+        master=master,
+        text=name,
+        relief=tk.RAISED,
+        borderwidth=4,
+        command=lambda: load_overview_page(master, name))
+
+class ProfileButton(tk.Button):
+
+    def __init__(self, name, *args, **kwargs):
+        tk.Button.__init__(self, *args, **kwargs)
+        self.name = name
+        self.become_normal()
+
+    def become_normal(self):
+        from windowController import load_overview_page
+        self.configure(
+            text=self.name,
+            relief=tk.RAISED,
+            bg="SystemButtonFace",
+            borderwidth=4,
+            command=lambda: load_overview_page(self.master, self.name))
+
+    def become_delete(self):
+        from windowController import load_profile_page
+        from dataManager import delete_profile
+        self.configure(
+            text = "DELETE -- " + self.name,
+            bg="red",
+            command=lambda: (delete_profile(self.name),
+                             load_profile_page(self.master)))
 
 
 class ProfileList(tk.Frame):
-    
+  
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
 
@@ -26,7 +51,7 @@ class ProfileList(tk.Frame):
         self.buttons = [None] * len(profile_names)
 
         for row, name in enumerate(profile_names):
-            self.buttons[row] = profile_button(self, name)
+            self.buttons[row] = ProfileButton(name, master=self)
             self.buttons[row].grid(
                                 row=row,
                                 column=0,
@@ -34,29 +59,61 @@ class ProfileList(tk.Frame):
                                 padx=3,
                                 pady=3)
 
+    def to_normal(self):
+        for button in self.buttons:
+            button.become_normal()
+
+
+    def to_delete(self):
+        for button in self.buttons:
+            button.become_delete()
+
+
+class ProfilePage(tk.Frame):
+
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        
+        self.columnconfigure(0, weight=1, minsize=20)
+        self.columnconfigure(1, weight=1, minsize=100)
+        self.columnconfigure(2, weight=1, minsize=20)
+        
+        fastmath_label = tk.Label(master=self, text="Fast Math")
+        fastmath_label.grid(row=0, column=1)
+
+        select_profile_label = tk.Label(master=self, text="Select Profile")
+        select_profile_label.grid(row=1, column=1, sticky=tk.W)
+
+        profiles = ProfileList(master=self)
+        profiles.grid(row=2, column=1, sticky=tk.E+tk.W)
+
+        from windowController import load_create_profile_page
+        create_profile_button = tk.Button(
+            master=self,
+            text="Create New Profile",
+            command=lambda: load_create_profile_page(self.master))
+        create_profile_button.grid(row=3, column=1, sticky=tk.W+tk.E)
+
+        delete_profile_button = tk.Button(master=self)
+
+        def handle_cancel_click():
+            delete_profile_button.configure(
+                    text="Delete Existing Profile",
+                    bg="red",
+                    command=handle_delete_click)
+            profiles.to_normal()
+        
+        def handle_delete_click():
+            delete_profile_button.configure(
+                    text="Cancel",
+                    bg="green",
+                    command=handle_cancel_click)
+            profiles.to_delete()
+            
+        handle_cancel_click()
+        delete_profile_button.grid(row=4, column=1, sticky=tk.W+tk.E)
+
 # Returns the root frame of the profile page
 def profile_page(master):
-    from windowController import load_create_profile_page
+    return ProfilePage(master=master)
 
-    root = tk.Frame(master=master)
-
-    root.columnconfigure(0, weight=1, minsize=20)
-    root.columnconfigure(1, weight=1, minsize=100)
-    root.columnconfigure(2, weight=1, minsize=20)
-
-    fastmath_label = tk.Label(master=root, text="Fast Math")
-    fastmath_label.grid(row=0, column=1)
-
-    select_profile_label = tk.Label(master=root, text="Select Profile")
-    select_profile_label.grid(row=1, column=1, sticky=tk.W)
-
-    profiles = ProfileList(master=root)
-    profiles.grid(row=2, column=1, sticky=tk.E+tk.W)
-
-    create_profile_button = tk.Button(
-        master=root,
-        text="Create New Profile",
-        command=lambda: load_create_profile_page(master))
-    create_profile_button.grid(row=3, column=1, sticky=tk.W+tk.E)
-
-    return root
